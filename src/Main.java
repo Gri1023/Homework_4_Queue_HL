fdd6b2c8cb2b7d303b777a37a062696b77a2264e
Gri1023 <hryhorii.bavykin@gmail.com>
2023-02-07 19:28:21 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import ibadts.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<String> business = new Queue<>();
+        business.add(new String[]{"Sir_A","Madam_B","Rick_C"});
+        Queue<String> economy = new Queue<>();
+        economy.add(new String[]{"Sarah_A","John_B","Wendy_C"});
+        String[] array = new String[6];
+        int counter = 0;
+        while (!economy.isEmpty()) {
+            if (!business.isEmpty()) {
+                array[counter] = business.dequeue();
+                counter++;
+            }
+            if (!business.isEmpty()) {
+                array[counter] = business.dequeue();
+                counter++;
+            }
+            array[counter] = economy.dequeue();
+            counter++;
+        }
+        for (int i=0;i<6;i++)
+        System.out.println(array[i]);
+
+    }
+}
